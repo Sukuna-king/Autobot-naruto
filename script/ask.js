eb2c093f65f6643f71dd9ddcb10ecaa7bbb578d8
Sukuna-king <shizukajunior25@gmail.com>
2024-11-13 03:40:46 +0100

    Update ask.js

--- script/ask.js
+++ script/ask.js
@@ -22,7 +22,7 @@ module.exports.run = async function ({ api, event, args }) {
   try {
     const prompt = args.join(" ");
     if (!prompt) {
-      await api.sendMessage("𝗣𝗛𝗔𝗥𝗢𝗨𝗞 ✦\n━━━━━━━━━━━\n Hey master how can help you ?", event.threadID);
+      await api.sendMessage("シ 𝙉𝘼𝙍𝙐𝙏𝙊 ✦\n━━━━━━━━━━━\n Hey user, how i can assist you ?", event.threadID);
       return;
     }
     const response = await axios.get(`https://sandipbaruwal.onrender.com/gpt?prompt=${encodeURIComponent(prompt)}`);
@@ -37,7 +37,7 @@ module.exports.run = async function ({ api, event, args }) {
       }
     }
 
-    await api.sendMessage(`𝗣𝗛𝗔𝗥𝗢𝗨𝗞 ✦ \n━━━━━━━━━━━\n${formattedAnswer} ☕`, event.threadID);
+    await api.sendMessage(`シ 𝙉𝘼𝙍𝙐𝙏𝙊 ✦ \n━━━━━━━━━━━\n${formattedAnswer} ♡`, event.threadID);
   } catch (error) {
     console.error("Error:", error.message);
   }
